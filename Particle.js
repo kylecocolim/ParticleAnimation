483b4f374aa5ce3bed4283af6a2ea9cee319c2e0
임진택 <kococo9504@gmail.com>
2021-02-07 11:10:31 +0900

    add circle random position" "

--- Particle.js
+++ Particle.js
@@ -1,6 +1,6 @@
 class Particle{
     constructor(circleCount=0, radius=0){
-        this.debug = true
+        this.debug = false
         this.circleCount = circleCount
         this.radius = radius
     }
@@ -13,18 +13,38 @@ class Particle{
         }
         if(canvas){
             const ctx = canvas.getContext('2d');
-            ctx.canvas.width = window.innerWidth;
-            ctx.canvas.height = window.innerHeight;
-            this.drawCircle(ctx);
+            const tick = 3000
+            ctx.canvas.width = window.outerHeight;
+            ctx.canvas.height = window.outerHeight;
+            if(this.debug){
+                this.circleCount = 1
+            }
+            for(let i=0; i<this.circleCount;i++){
+                let xPos = (Math.random()*tick)%window.outerHeight;
+                let yPos = (Math.random()*tick)%window.outerHeight;
+                console.log(xPos,yPos);
+                this.drawCircle(ctx,xPos,yPos);
+            }
         }
     }
-    drawCircle(ctx){
+    drawCircle(ctx,xPos,yPos){
+        const tick = 10000;
+        const red = (Math.random() * tick) % 255;
+        const blue = (Math.random() * tick) % 255;
+        const green = (Math.random() * tick) % 255
+        if(this.debug){
+            xPos = 100
+            yPos = 100
+        }
         ctx.beginPath();
-        ctx.arc(100, 75, 50, 0, 2 * Math.PI);
-        ctx.stroke();
+        ctx.arc(xPos, yPos, this.radius, 0, 2 * Math.PI);
+        ctx.fillStyle = `rgba(${red},${green},${blue},1)`
+        ctx.fill();
+        
+        
     }
 }
-CIRCLECOUNT = 10;
-RADIUS = 10;
+CIRCLECOUNT = 3000;
+RADIUS = 20;
 particle = new Particle(CIRCLECOUNT,RADIUS);
 particle.init();
